2df3ed5bf9b137d04f2747f0bba9bdb8c785dd45
asagix <multiple12@gmail.com>
2017-12-25 21:49:56 +0900

    ユニットテスト追加

---
+++ UnitTest/ExcelProcessorUnitTest.cs
@@ -0,0 +1,26 @@
+﻿namespace UnitTest
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Subtractive.Processor;
+    using System.IO;
+
+    [TestClass]
+    public class ExcelProcessorUnitTest
+    {
+        [TestMethod,TestCategory("ExcelProcessor")]
+        public void ExecuteTestFromPngXlsx()
+        {
+            ExcelProcessor processor = new ExcelProcessor();
+            processor.Execute(@"Resources\pngBook.xlsx");
+            Assert.IsTrue(File.Exists(@"Resources\減色済pngBook.xlsx"));
+        }
+
+        [TestMethod, TestCategory("ExcelProcessor")]
+        public void ExecuteTestFromPngXlsm()
+        {
+            ExcelProcessor processor = new ExcelProcessor();
+            processor.Execute(@"Resources\pngBook.xlsm");
+            Assert.IsTrue(File.Exists(@"Resources\減色済pngBook.xlsm"));
+        }
+    }
+}
---
+++ UnitTest/ZipProcessorUnitTest.cs
@@ -0,0 +1,27 @@
+﻿namespace UnitTest
+{
+    using System;
+    using System.IO;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Subtractive.Processor;
+
+    [TestClass]
+    public class ZipProcessorUnitTest
+    {
+        ZipProcessor processor = new ZipProcessor();
+
+        [TestMethod]
+        public void ExecuteTestFromPng()
+        {
+            processor.Execute(@"Resources\jpeg01.zip");
+            Assert.IsTrue(File.Exists(@"Resources\減色済jpeg01.zip"));
+        }
+
+        [TestMethod]
+        public void ExecuteTestFromJpeg()
+        {
+            processor.Execute(@"Resources\png01.zip");
+            Assert.IsTrue(File.Exists(@"Resources\減色済png01.zip"));
+        }
+    }
+}
